9a994082700c39f0df954110f213962a09c71608
ABgithub1 <Mcformytable@0utlook.com>
2021-11-04 22:13:20 +0300

    03.11.21

--- src/main/java/by/academy/homework1/Task4.java
+++ src/main/java/by/academy/homework1/Task4.java
@@ -3,14 +3,13 @@ package by.academy.homework1;
 public class Task4 {
     public static void main(String[] args) {
         int res = 0;
-        int degree = 1;
+        int num = 1;
         System.out.println("Возвожу число 2 в степени");
         System.out.println("Степень: 0  Любое число в нулевой степени равно 1 (Кроме нуля :))");
         for (int i = 1; res < 1_000_000; i++) {
-            res = i * 2;
-            i = res;
-            degree++;
-            System.out.println("Степень: " + degree + " " + " Результат возведения: " + res);
+            res = num * 2;
+            num = res;
+            System.out.println("Степень: " + i + " " + " Результат возведения: " + res);
         }
     }
 }
