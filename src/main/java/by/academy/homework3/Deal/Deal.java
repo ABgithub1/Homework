756b3fec381c8d44920cd2db466759d32779b17c
ABgithub1 <Mcformytable@outlook.com>
2021-11-25 12:46:52 +0300

    23.11.21

--- src/main/java/by/academy/homework3/Deal/Deal.java
+++ src/main/java/by/academy/homework3/Deal/Deal.java
@@ -43,7 +43,7 @@ public class Deal {
         }
         System.out.println("_______________________________________");
         System.out.println("К оплате: " + getFullPrice() + " рублей");
-        System.out.println("Покупка состоялась, остаток: " + buyer.getMoney());
+        System.out.println("Остаток: " + buyer.getMoney());
     }
 
 
